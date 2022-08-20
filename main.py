586abe4a80fbb570c254ec3968c78c37a90537e9
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-08-20 05:45:08 +0200

    Done, some small fixes will be needed yet

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from typing import Optional
 from threading import Timer
 from enum import Enum
 import pickle
+import random
 
 import selenium.webdriver.remote.webelement
 from selenium import webdriver
@@ -17,6 +18,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import Select
 
 from dotenv import load_dotenv
 
@@ -24,6 +26,7 @@ load_dotenv()
 
 from refactor_info_txt import refactor_info_txt
 
+# BUG: Some images are rotated
 
 class Settings:
     COOKIES_FILE = 'cookies.pkl'
@@ -66,6 +69,10 @@ class RepeatedTimer(object):
         self.is_running = False
 
 
+def wait_random_time():
+    time.sleep(random.random() * 1)
+
+
 def wait_n_seconds(seconds: int):
     for i in range(seconds):
         time.sleep(1)
@@ -82,13 +89,8 @@ product_info = {
     'rubric': '>>RUBRIC',
     'category': '>>CATEGORY',
     'title': '>>TITLE',
-    'description': '>>DESCRIPTION',
     'price': '>>PRICE',
-    'psc': '>>PSC',
-    'name': '>>NAME',
-    'phone_number': '>>PHONE_NUMBER',
-    'email': '>>EMAIL',
-    'password': '>>PASSWORD'
+    'description': '>>DESCRIPTION',
 }
 
 
@@ -97,13 +99,11 @@ class Product:
         self.rubric = ''
         self.category = ''
         self.title = ''
-        self.description = ''
         self.price = ''
-        self.psc = ''
-        self.name = ''
-        self.phone_number = ''
-        self.email = ''
-        self.password = ''
+        self.description = ''
+        self.images = sorted(map(lambda x: path.join(product_path, 'photos', x),
+                          next(os.walk(path.join(product_path, 'photos')))[2]))
+        #
         self.load_product(product_dir=product_path)
 
     def get_current_section(self, line) -> str:
@@ -115,51 +115,26 @@ class Product:
 
     def load_product(self, *, product_dir):
         # NOTE: Rewrite this shit code
-
-        print(product_dir)
         with open(file=path.join(product_dir, 'info.txt'), mode='r') as file:
-            _continue = False
             for line in file.readlines():
-                # get section
                 if '>>' in line:
                     current_product_info_key = self.get_current_section(line=line)
-                    _continue = False
                     continue
 
-                # NOTE: if section should be only one line text continue until next section
-                if _continue: continue
+                if line.replace(' ', '').replace('\t', '').replace('\n', '') == '':
+                    continue
 
-                # save
                 if product_info[current_product_info_key] == product_info['rubric']:
                     self.rubric = line.strip()
-                    _continue = True
                 elif product_info[current_product_info_key] == product_info['category']:
                     self.category = line.strip()
-                    _continue = True
                 elif product_info[current_product_info_key] == product_info['title']:
                     self.title = line.strip()
-                    _continue = True
-                elif product_info[current_product_info_key] == product_info['description']:
-                    self.description += line.strip()  # NOTE: Only here we appending line
-                    _continue = True
                 elif product_info[current_product_info_key] == product_info['price']:
                     self.price = line.strip()
-                    _continue = True
-                elif product_info[current_product_info_key] == product_info['psc']:
-                    self.psc = line.strip()
-                    _continue = True
-                elif product_info[current_product_info_key] == product_info['name']:
-                    self.name = line.strip()
-                    _continue = True
-                elif product_info[current_product_info_key] == product_info['phone_number']:
-                    self.phone_number = line.strip()
-                    _continue = True
-                elif product_info[current_product_info_key] == product_info['email']:
-                    self.email = line.strip()
-                    _continue = True
-                elif product_info[current_product_info_key] == product_info['password']:
-                    self.password = line.strip()
-                    _continue = True
+                # NOTE: Only here we appending line, must be at the end of info.txt
+                elif product_info[current_product_info_key] == product_info['description']:
+                    self.description += line
 
 
 class BazosScrapper:
@@ -173,61 +148,91 @@ class BazosScrapper:
 
         # Prepare authentication
         telefon_input = self.driver.find_element(By.NAME, 'telefon')
+        telefon_input.clear()
         telefon_input.send_keys(user.phone_number)
         click_submit_by_value(submits=self.driver.find_elements(By.NAME, 'Submit'), value='Ověřit')
 
         # Authenticate
         code_input = self.driver.find_element(By.NAME, 'klic')
+        code_input.clear()
         code_input.send_keys(input('Please provide authentification code sended to your phone: '))
         click_submit_by_value(submits=self.driver.find_elements(By.NAME, 'Submit'), value='Odeslat')
-
-        wait_n_seconds(20)
 
         # Save cookies
         pickle.dump(self.driver.get_cookies(), open(Settings.COOKIES_FILE, "wb"))
         # Save Local Storage
         pickle.dump(self.driver.execute_script("return window.localStorage;"), open(Settings.LOCAL_STORAGE_FILE, "wb"))
 
+    def remove_advertisment(self, user: User):
+        self.driver.find_element(By.CLASS_NAME, 'inzeratydetdel').find_element(By.TAG_NAME, 'a').click()
+        self.driver.find_element(By.NAME, 'heslobazar').clear()
+        self.driver.find_element(By.NAME, 'heslobazar').send_keys(user.pasword)
+        click_submit_by_value(self.driver.find_elements(By.NAME, 'administrace'), 'Vymazat')
 
-    # def load_my_advertisements(self):
-    def remove_all_advertisements(self):
-        # TODO
-        pass
+    def remove_advertisements(self, user: User):
+        self.load_page_with_cookies(url=self.url_moje_inzeraty)
+        self.driver.get(self.url_moje_inzeraty)
+        while len(self.driver.find_elements(By.CLASS_NAME, 'nadpis')) != 0:
+            wait_random_time()
+            self.driver.find_element(By.CLASS_NAME, 'nadpis').find_element(By.TAG_NAME, 'a').click()
+            wait_random_time()
+            self.remove_advertisment(user=user)
+            wait_random_time()
 
-    def add_advertisement(self, path_dir: str, product_info: Product):
-        icons = self.driver.find_elements(By.CLASS_NAME, 'iconstblcell')
-        for icon in icons:
-            b = icon.find_element(By.TAG_NAME, 'a').find_element(By.TAG_NAME, 'b')
-            if b.text == product_info.rubric:
-                icon.click()
-                # TODO: I stuck here because "cookies or something else not working" and it need mobile check every time
-                wait_n_seconds(seconds=5)
+    def add_advertisement(self, product: Product, user: User):
+        Select(self.driver.find_element(By.NAME, 'rubrikyvybrat')).select_by_visible_text(product.rubric)
+        Select(self.driver.find_element(By.ID, 'category')).select_by_visible_text(product.category)
+
+        wait_random_time()
+        self.driver.find_element(By.ID, 'nadpis').send_keys(product.title)
+        self.driver.find_element(By.ID, 'popis').send_keys(product.description)
+        self.driver.find_element(By.ID, 'cena').send_keys(product.price)
+
+        wait_random_time()
+        self.driver.find_element(By.ID, 'lokalita').clear()
+        self.driver.find_element(By.ID, 'lokalita').send_keys(user.psc)
+        self.driver.find_element(By.ID, 'jmeno').clear()
+        self.driver.find_element(By.ID, 'jmeno').send_keys(user.name)
+        self.driver.find_element(By.ID, 'telefoni').clear()
+        self.driver.find_element(By.ID, 'telefoni').send_keys(user.phone_number)
+        self.driver.find_element(By.ID, 'maili').clear()
+        self.driver.find_element(By.ID, 'maili').send_keys(user.email)
+        self.driver.find_element(By.ID, 'heslobazar').clear()
+        self.driver.find_element(By.ID, 'heslobazar').send_keys(user.pasword)
+
+        wait_random_time()
+        self.driver.find_element(By.CLASS_NAME, 'ovse').click()
+        self.driver.find_element(By.NAME, 'souborp[]').send_keys('\n'.join(product.images))
+
+        wait_random_time()
+        click_submit_by_value(submits=self.driver.find_elements(By.NAME, 'Submit'), value='Odeslat')
+
+    def go_to_rubric(self, product: Product):
+        sections = self.driver.find_elements(By.CLASS_NAME, 'iconstblcell')
+        for icon in sections:
+            a = icon.find_element(By.TAG_NAME, 'a')
+            if a.accessible_name == product.rubric:
+                a.click()
+                break
 
     def add_advertisements(self, user: User):
-        # Prepare page
         self.load_page_with_cookies(url=self.url_moje_inzeraty)
-        self.driver.find_element(By.CLASS_NAME, 'pridati').click()  # go to add page
-        wait_n_seconds(10)
+        for dir in os.listdir(path=user.products_path):
+            print(dir)
 
-        # Start adding
-        # for dir in os.listdir(path=user.products_path):
-        #     product_path = path.join(user.products_path, dir)
-        #     product = Product(product_path=product_path)
-        #     print(product.name)
-        #     self.add_advertisement(path_dir=product_path, product_info=product)
-        #     break
+            wait_random_time()
+            self.driver.find_element(By.CLASS_NAME, 'pridati').click()  # go to add page
+            product_path = path.join(user.products_path, dir)
+            product = Product(product_path=product_path)
 
-    def load_page_with_cookies(self, url: str):
-        self.driver.get(url)
-        # for cookie_dict in pickle.load(open('cookies.pkl', 'rb')):
-        #     for key, value in cookie_dict.items():
-        #         print({"name": key, "value": value})
-                # self.driver.add_cookie({"name": key, "value": value})
-            # break
+            wait_random_time()
+            self.go_to_rubric(product=product)
+            self.add_advertisement(product=product, user=user)
 
-        for key, value in pickle.load(open('cookies.pkl', 'rb'))[-1].items():
-                print({"name": key, "value": value})
-            # self.driver.add_cookie({"name": key, "value": value})
+    def load_page_with_cookies(self, url: str = ''):
+        self.driver.get(self.url_moje_inzeraty)
+        for cookie_dict in pickle.load(open('cookies.pkl', 'rb')):
+            self.driver.add_cookie(cookie_dict)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.driver.quit()
@@ -243,11 +248,14 @@ def main():
 
     # Start advertising
     bazos_scrapper = BazosScrapper()
+
+    # Prepare Cookies
     if not os.path.isfile(Settings.COOKIES_FILE) or not os.path.isfile(Settings.LOCAL_STORAGE_FILE):
         bazos_scrapper.save_authentication(user=user)
-        print(pickle.load(open(Settings.COOKIES_FILE, 'rb')))
-        print(pickle.load(open(Settings.LOCAL_STORAGE_FILE, 'rb')))
-    # bazos_scrapper.add_advertisements(user=user)
+
+    # Restore advertisements
+    bazos_scrapper.remove_advertisements(user=user)
+    bazos_scrapper.add_advertisements(user=user)
 
 
 if __name__ == '__main__':
--- refactor_info_txt.py
+++ refactor_info_txt.py
@@ -2,15 +2,22 @@ from os import path
 import os
 
 
-def refactor_info_txt(url: str):
-    for dir in next(os.walk(url))[1]:  # loop through all directories
-        file = path.join(url, dir, 'info.txt')
+def refactor_info_txt(_path: str):
+    for dir in next(os.walk(_path))[1]:  # loop through all directories
+        file = path.join(_path, dir, 'info.txt')
 
         assert path.isfile(file), 'File not exist: info.txt'
 
         with open(file=file, mode='r') as f:
             lines = f.readlines()
-            idx = lines.index('>>PRICE\n') + 1
 
-        with open(file=file, mode='w') as f:
-            f.writelines(lines[:idx])
+        try:
+            idx_rubric = lines.index('>>RUBRIKA\n')
+            idx_category = lines.index('>>KATEGORIE\n')
+            # lines[idx_rubric] = '>>RUBRIC\n'
+            # lines[idx_category] = '>>CATEGORY\n'
+            # with open(file=file, mode='w') as f:
+            #     f.writelines(lines)
+        except:
+            pass
+
